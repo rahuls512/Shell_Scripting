6f67d2f1dc2fb70eaad4b7e94f1b3abb6ee39275
Rahul Sharan <123796550+rahuls512@users.noreply.github.com>
2023-09-06 19:16:52 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
-#Install Java
+# Install Java
 
-sudo apt update
-sudo apt install openjdk-11-jre
+sudo apt-get update
+sudo apt-get install fontconfig openjdk-11-jre
 
-#Now, you can proceed with installing Jenkins
+# This is the Debian package repository of Jenkins to automate installation and upgrade.
+# To use this repository, first add the key to your system:
 
-curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+    /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+    
+# Then add a Jenkins apt repository entry:
+
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
+    https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+    /etc/apt/sources.list.d/jenkins.list > /dev/null
+    
+# Update your local package index, then finally install Jenkins:
 sudo apt-get update
 sudo apt-get install jenkins
 
-#open port 8080
+# open port 8080
+# Jenkins Installation link: https://pkg.jenkins.io/debian-stable/
 
